e312f6fc8ff4fc7dc36b4c77a1edcbae5a28aa9d
rowleya-phut <andrew.rowley@porthosp.nhs.uk>
2020-12-02 11:15:37 +0000

    Update testreadwebserver.php

--- php/testreadwebserver.php
+++ php/testreadwebserver.php
@@ -3,8 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 
 include "config.php";
-// 2. Select a database to use 
-$db_select = mysqli_select_db($connection, $dbName);
+
 
 if (!$db_select) {
     die("Database selection failed: " . mysqli_error($connection));
